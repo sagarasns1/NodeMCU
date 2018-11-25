0a2fa3cb1c2ec6aff9d66461f728daf69de54742
Sagara Sumathipala <ssumathipala@gmail.com>
2018-11-25 09:15:56 +0530

    Add files via upload

---
+++ Node_Publishe2/Node_Publishe2.ino
@@ -0,0 +1,25 @@
+#include "DHT.h"
+
+DHT dht;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println();
+  Serial.println("Status\tHumidity (%)\tTemperature (C)\t(F)");
+  dht.setup(D1);   /* D1 is used for data communication */
+}
+
+void loop()
+{
+  delay(dht.getMinimumSamplingPeriod());/* Delay of amount equal to sampling period */
+  float humidity = dht.getHumidity();/* Get humidity value */
+  float temperature = dht.getTemperature();/* Get temperature value */
+  Serial.print(dht.getStatusString());/* Print status of communication */
+  Serial.print("\t");
+  Serial.print(humidity, 1);
+  Serial.print("\t\t");
+  Serial.print(temperature, 1);
+  Serial.print("\t\t");
+  Serial.println(dht.toFahrenheit(temperature), 1);/* Convert temperature to Fahrenheit units */
+}
